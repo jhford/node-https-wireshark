99e51e008f2bcedb7acc70412895f9c6643b22ae
Forest Johnson <fjohnson@peoplenetonline.com>
2018-11-02 14:02:46 -0500

    fix more bugs and remove single ticks so I can easily paste this in a bash multiline string literal

--- index.js
+++ index.js
@@ -1,30 +1,31 @@
 // NOTE: This code is based on the following Github Pull Request comment
 // https://github.com/nodejs/node/issues/2363#issuecomment-278498852
-var https = require('https');
+var https = require("https");
+var fs = require("fs");
 
-// This is from the original comment.  I dont' really know the format
-// that this is stored in, so I'm going to leave it here.  This should
+// This is from the original comment.  I dont really know the format
+// that this is stored in, so Im going to leave it here.  This should
 // be rewritten at some point.  The comment says that the buffer to
-// parse is a DER-encoded ASN.1 structure.  I don't know what that is
+// parse is a DER-encoded ASN.1 structure.  I dont know what that is
 // but this does work
 function parseSession(buf) {
   return {
-    sessionId: buf.slice(17, 17+32).toString('hex'),
-    masterKey: buf.slice(51, 51+48).toString('hex')
+    sessionId: buf.slice(17, 17+32).toString("hex"),
+    masterKey: buf.slice(51, 51+48).toString("hex")
   };
 }
 
 function patchRequest(req) {
-  req.once('socket', function(s) {
-    s.once('secureConnect', function() {
+  req.once("socket", function(s) {
+    s.once("secureConnect", function() {
       var session = parseSession(s.getSession());
       // session.sessionId and session.masterKey should be hex strings
       var id = session.sessionId;
       var key = session.masterKey;
-      var logline = 'RSA Session-ID:' + id + 'Master-Key:' + key + '\n';
+      var logline = "RSA Session-ID:" + id + "Master-Key:" + key + "\n";
       var logfile = process.env.SSLKEYLOGFILE;
       if (!logfile) {
-        console.log('Missing Environment Variable SSLKEYLOGFILE');
+        console.log("Missing Environment Variable SSLKEYLOGFILE");
       }
       fs.appendFileSync(logfile, logline);
     });
